501e419468b4b07116553283b0b49084d1553ca7
Ronald Cron <ronald.cron@arm.com>
2022-02-10 14:50:54 +0100

    Allow hybrid TLS 1.3 + TLS 1.2 configuration
    Signed-off-by: Ronald Cron <ronald.cron@arm.com>

--- library/ssl_tls.c
+++ library/ssl_tls.c
@@ -872,8 +872,13 @@ static int ssl_conf_version_check( const mbedtls_ssl_context *ssl )
 #if defined(MBEDTLS_SSL_PROTO_TLS1_2) && defined(MBEDTLS_SSL_PROTO_TLS1_3)
     if( mbedtls_ssl_conf_is_hybrid_tls12_tls13( ssl->conf ) )
     {
-        MBEDTLS_SSL_DEBUG_MSG( 1, ( "Hybrid TLS 1.2 + TLS 1.3 configurations are not yet supported" ) );
-        return( MBEDTLS_ERR_SSL_FEATURE_UNAVAILABLE );
+        if( ssl->conf->transport == MBEDTLS_SSL_TRANSPORT_DATAGRAM )
+        {
+             MBEDTLS_SSL_DEBUG_MSG( 1, ( "DTLS not yet supported in Hybrid TLS 1.3 + TLS 1.2" ) );
+             return( MBEDTLS_ERR_SSL_FEATURE_UNAVAILABLE );
+        }
+        MBEDTLS_SSL_DEBUG_MSG( 4, ( "The SSL configuration is TLS 1.3 or TLS 1.2." ) );
+        return( 0 );
     }
 #endif
 
@@ -2732,8 +2737,11 @@ int mbedtls_ssl_handshake_step( mbedtls_ssl_context *ssl )
     if( ssl->conf->endpoint == MBEDTLS_SSL_IS_CLIENT )
     {
 #if defined(MBEDTLS_SSL_PROTO_TLS1_3)
-        if( mbedtls_ssl_conf_is_tls13_only( ssl->conf ) )
+        if( mbedtls_ssl_conf_is_tls13_only( ssl->conf ) ||
+            mbedtls_ssl_conf_is_hybrid_tls12_tls13( ssl->conf ) )
+        {
             ret = mbedtls_ssl_tls13_handshake_client_step( ssl );
+        }
 #endif /* MBEDTLS_SSL_PROTO_TLS1_3 */
 
 #if defined(MBEDTLS_SSL_PROTO_TLS1_2)
