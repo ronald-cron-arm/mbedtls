ca62843b8e70225b0931d205bb0102504b8ba8ca
Ronald Cron <ronald.cron@arm.com>
2024-07-24 10:02:07 +0200

    WIP
    Signed-off-by: Ronald Cron <ronald.cron@arm.com>

---
+++ tf-psa-crypto/include/tf_psa_crypto/version.h
@@ -0,0 +1,7 @@
+/**
+ * The version number x.y.z is split into three parts.
+ * Major, Minor, Patchlevel
+ */
+#define TF_PSA_CRYPTO_VERSION_MAJOR  0
+#define TF_PSA_CRYPTO_VERSION_MINOR  1
+#define TF_PSA_CRYPTO_VERSION_PATCH  0
